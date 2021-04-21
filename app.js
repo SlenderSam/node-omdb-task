a6611fbd4b1478afa8afde554cf550473e625f05
Samuel <sblokland@live.com.au>
2021-04-21 18:56:01 +0930

    OMDb JSON data working on function but cannot interact
    A base web page has been set up and a javascript function allows json data to be logged to console from OMDb. However, so far current set up does not support searching or returning OMDb information to the web page.

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
+const { response } = require('express');
 const express = require('express');
+const fetch = require('node-fetch');
 
 // express app
 const app = express();
 
+async function searchApi(){
+    const response = await fetch('http://www.omdbapi.com/?apikey=12b69f05&s=star+wars')
+    
+    var data = await response.json();
+    console.log(data);
+    return data;
+}
+
+// connect to OMDb w/ API key
+const dbURI = 'http://www.omdbapi.com/?apikey=12b69f05&'
+// search by title:
+    // http://www.omdbapi.com/?apikey=12b69f05&t=lion+king
+// search by id (obtain from title JSON if unknown):
+    // http://www.omdbapi.com/?apikey=12b69f05&i=tt0110357
+// obtain poster image:
+    // http://img.omdbapi.com/?apikey=12b69f05&i=tt0110357
+
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/main.html', { root: __dirname })
-});
-
-app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname })
-});
-
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
-
-// 404 page
-app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    const blogs = [
+        {title: 'help', snippet: 'Lorem ipsum dotor sit amet consectetur'},
+        {title: 'me', snippet: 'Lorem ipsum dotor sit amet consectetur'},
+        {title: 'please', snippet: 'Lorem ipsum dotor sit amet consectetur'},
+    ]
+    const movies = searchApi();
+    res.render('mainSearch', {blogs, movies});
 });
